9fa42bce5f5ee9075b89eab0429de684b7a36cb6
Steve Aschwanden <steve.aschwanden@biel-bienne.ch>
2015-05-18 09:06:12 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+
+version = '1.0'
+
+long_description = (
+    open('README.rst').read()
+    + '\n' +
+    'Contributors\n'
+    '============\n'
+    + '\n' +
+    open('CONTRIBUTORS.txt').read()
+    + '\n' +
+    open('CHANGES.txt').read()
+    + '\n')
+
+setup(name='bielbienne.demo',
+      version=version,
+      description="Demo package",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Steve Aschwanden',
+      author_email='steve.aschwanden@biel-bienne.ch',
+      url='',
+      license='gpl',
+      packages=find_packages('src'),
+      package_dir = {'': 'src'},
+      namespace_packages=['bielbienne'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'pyaxl',
+          'bb.extjs.core',
+          'pyaml',
+          'Whoosh',
+          'Unidecode'
+      ],
+      entry_points={
+      },
+      )
